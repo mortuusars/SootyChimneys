8c86a2ba4390ac0c1951cfbc1cc850db84c8fa82
mortuusars <mortuusars@gmail.com>
2022-06-26 16:36:25 +0300

    Fixed client crash when teleporting away from chimneys. Fixes #3.

--- src/main/java/io/github/mortuusars/sootychimneys/blocks/ChimneyBlockEntity.java
+++ src/main/java/io/github/mortuusars/sootychimneys/blocks/ChimneyBlockEntity.java
@@ -1,11 +1,13 @@
 package io.github.mortuusars.sootychimneys.blocks;
 
+import com.mojang.logging.LogUtils;
 import io.github.mortuusars.sootychimneys.config.CommonConfig;
 import io.github.mortuusars.sootychimneys.setup.ModBlockEntities;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import org.slf4j.Logger;
 
 public class ChimneyBlockEntity extends BlockEntity {
     public ChimneyBlockEntity(BlockPos pWorldPosition, BlockState pBlockState) {
@@ -13,7 +15,8 @@ public class ChimneyBlockEntity extends BlockEntity {
     }
 
     public static <T extends BlockEntity> void particleTick(Level level, BlockPos blockPos, BlockState blockState, T t) {
-        if (level.getRandom().nextDouble(0.0001d, 1.0d) < CommonConfig.SMOKE_STRENGTH.get())
-            ((ChimneyBlock)level.getBlockState(blockPos).getBlock()).emitParticles(level, blockPos, blockState);
+        if (level.getRandom().nextDouble(0.0001d, 1.0d) < CommonConfig.SMOKE_STRENGTH.get()
+                && blockState.getBlock() instanceof ChimneyBlock chimney)
+            chimney.emitParticles(level, blockPos, blockState);
     }
 }
