77dba9d45b4a6778314dfb7e2bb27a782d5c51ec
mortuusars <mortuusars@gmail.com>
2025-03-02 03:51:20 +0200

    Create - smoke on contraptions

--- neoforge/src/main/java/io/github/mortuusars/sootychimneys/neoforge/event/CommonEvents.java
+++ neoforge/src/main/java/io/github/mortuusars/sootychimneys/neoforge/event/CommonEvents.java
@@ -1,10 +1,13 @@
 package io.github.mortuusars.sootychimneys.neoforge.event;
 
 import io.github.mortuusars.sootychimneys.SootyChimneys;
+import io.github.mortuusars.sootychimneys.neoforge.integration.create.CreateIntegration;
 import net.minecraft.stats.Stats;
 import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.ModList;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.fml.event.lifecycle.InterModEnqueueEvent;
 
 @SuppressWarnings("unused")
 @EventBusSubscriber(modid = SootyChimneys.ID, bus = EventBusSubscriber.Bus.MOD)
@@ -16,6 +19,15 @@ public class CommonEvents {
             SootyChimneys.Stats.STATS.forEach((location, statFormatter) -> {
                 Stats.CUSTOM.get(location);
             });
+        });
+    }
+
+    @SubscribeEvent
+    public static void onEnqueueIMC(InterModEnqueueEvent event) {
+        event.enqueueWork(() -> {
+            if (ModList.get().isLoaded("create")) {
+                CreateIntegration.registerMovingBehaviors();
+            }
         });
     }
 }
---
+++ neoforge/src/main/java/io/github/mortuusars/sootychimneys/neoforge/integration/create/CreateIntegration.java
@@ -0,0 +1,25 @@
+package io.github.mortuusars.sootychimneys.neoforge.integration.create;
+
+import com.simibubi.create.api.behaviour.movement.MovementBehaviour;
+import io.github.mortuusars.sootychimneys.SootyChimneys;
+
+public class CreateIntegration {
+    public static void registerMovingBehaviors() {
+        ChimneyMovementBehaviour chimneyMovementBehaviour = new ChimneyMovementBehaviour();
+
+        MovementBehaviour.REGISTRY.register(SootyChimneys.Blocks.BRICK_CHIMNEY.get(), chimneyMovementBehaviour);
+        MovementBehaviour.REGISTRY.register(SootyChimneys.Blocks.DIRTY_BRICK_CHIMNEY.get(), chimneyMovementBehaviour);
+        MovementBehaviour.REGISTRY.register(SootyChimneys.Blocks.COBBLESTONE_CHIMNEY.get(), chimneyMovementBehaviour);
+        MovementBehaviour.REGISTRY.register(SootyChimneys.Blocks.DIRTY_COBBLESTONE_CHIMNEY.get(), chimneyMovementBehaviour);
+        MovementBehaviour.REGISTRY.register(SootyChimneys.Blocks.STONE_BRICK_CHIMNEY.get(), chimneyMovementBehaviour);
+        MovementBehaviour.REGISTRY.register(SootyChimneys.Blocks.DIRTY_STONE_BRICK_CHIMNEY.get(), chimneyMovementBehaviour);
+        MovementBehaviour.REGISTRY.register(SootyChimneys.Blocks.MUD_BRICK_CHIMNEY.get(), chimneyMovementBehaviour);
+        MovementBehaviour.REGISTRY.register(SootyChimneys.Blocks.DIRTY_MUD_BRICK_CHIMNEY.get(), chimneyMovementBehaviour);
+        MovementBehaviour.REGISTRY.register(SootyChimneys.Blocks.IRON_CHIMNEY.get(), chimneyMovementBehaviour);
+        MovementBehaviour.REGISTRY.register(SootyChimneys.Blocks.DIRTY_IRON_CHIMNEY.get(), chimneyMovementBehaviour);
+        MovementBehaviour.REGISTRY.register(SootyChimneys.Blocks.COPPER_CHIMNEY.get(), chimneyMovementBehaviour);
+        MovementBehaviour.REGISTRY.register(SootyChimneys.Blocks.DIRTY_COPPER_CHIMNEY.get(), chimneyMovementBehaviour);
+        MovementBehaviour.REGISTRY.register(SootyChimneys.Blocks.TERRACOTTA_CHIMNEY.get(), chimneyMovementBehaviour);
+        MovementBehaviour.REGISTRY.register(SootyChimneys.Blocks.DIRTY_TERRACOTTA_CHIMNEY.get(), chimneyMovementBehaviour);
+    }
+}
