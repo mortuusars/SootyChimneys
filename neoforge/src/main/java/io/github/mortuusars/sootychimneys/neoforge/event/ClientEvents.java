3e0f701f530f690a10dd8655b8bee6d2df2c46c6
mortuusars <mortuusars@gmail.com>
2026-05-12 14:39:02 +0300

    Move creative tabs event. Probably does nothing.

--- neoforge/src/main/java/io/github/mortuusars/sootychimneys/neoforge/event/ClientEvents.java
+++ neoforge/src/main/java/io/github/mortuusars/sootychimneys/neoforge/event/ClientEvents.java
@@ -11,29 +11,6 @@ import net.neoforged.neoforge.event.tick.LevelTickEvent;
 
 @SuppressWarnings("unused")
 public class ClientEvents {
-    @EventBusSubscriber(modid = SootyChimneys.ID, bus = EventBusSubscriber.Bus.MOD)
-    public static class Mod {
-        @SubscribeEvent
-        public static void onCreativeTabsBuild(BuildCreativeModeTabContentsEvent event) {
-            if (event.getTabKey() == CreativeModeTabs.FUNCTIONAL_BLOCKS) {
-                event.accept(SootyChimneys.Items.BRICK_CHIMNEY.get());
-                event.accept(SootyChimneys.Items.DIRTY_BRICK_CHIMNEY.get());
-                event.accept(SootyChimneys.Items.COBBLESTONE_CHIMNEY.get());
-                event.accept(SootyChimneys.Items.DIRTY_COBBLESTONE_CHIMNEY.get());
-                event.accept(SootyChimneys.Items.STONE_BRICK_CHIMNEY.get());
-                event.accept(SootyChimneys.Items.DIRTY_STONE_BRICK_CHIMNEY.get());
-                event.accept(SootyChimneys.Items.MUD_BRICK_CHIMNEY.get());
-                event.accept(SootyChimneys.Items.DIRTY_MUD_BRICK_CHIMNEY.get());
-                event.accept(SootyChimneys.Items.IRON_CHIMNEY.get());
-                event.accept(SootyChimneys.Items.DIRTY_IRON_CHIMNEY.get());
-                event.accept(SootyChimneys.Items.COPPER_CHIMNEY.get());
-                event.accept(SootyChimneys.Items.DIRTY_COPPER_CHIMNEY.get());
-                event.accept(SootyChimneys.Items.TERRACOTTA_CHIMNEY.get());
-                event.accept(SootyChimneys.Items.DIRTY_TERRACOTTA_CHIMNEY.get());
-            }
-        }
-    }
-
     @EventBusSubscriber(modid = SootyChimneys.ID, bus = EventBusSubscriber.Bus.GAME)
     public static class Game {
         @SubscribeEvent
--- neoforge/src/main/java/io/github/mortuusars/sootychimneys/neoforge/event/CommonEvents.java
+++ neoforge/src/main/java/io/github/mortuusars/sootychimneys/neoforge/event/CommonEvents.java
@@ -3,15 +3,37 @@ package io.github.mortuusars.sootychimneys.neoforge.event;
 import io.github.mortuusars.sootychimneys.SootyChimneys;
 import io.github.mortuusars.sootychimneys.neoforge.integration.create.CreateIntegration;
 import net.minecraft.stats.Stats;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.ModList;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.neoforged.fml.event.lifecycle.InterModEnqueueEvent;
+import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
 
 @SuppressWarnings("unused")
 @EventBusSubscriber(modid = SootyChimneys.ID, bus = EventBusSubscriber.Bus.MOD)
 public class CommonEvents {
+    @SubscribeEvent
+    public static void onCreativeTabsBuild(BuildCreativeModeTabContentsEvent event) {
+        if (event.getTabKey() == CreativeModeTabs.FUNCTIONAL_BLOCKS) {
+            event.accept(SootyChimneys.Items.BRICK_CHIMNEY.get());
+            event.accept(SootyChimneys.Items.DIRTY_BRICK_CHIMNEY.get());
+            event.accept(SootyChimneys.Items.COBBLESTONE_CHIMNEY.get());
+            event.accept(SootyChimneys.Items.DIRTY_COBBLESTONE_CHIMNEY.get());
+            event.accept(SootyChimneys.Items.STONE_BRICK_CHIMNEY.get());
+            event.accept(SootyChimneys.Items.DIRTY_STONE_BRICK_CHIMNEY.get());
+            event.accept(SootyChimneys.Items.MUD_BRICK_CHIMNEY.get());
+            event.accept(SootyChimneys.Items.DIRTY_MUD_BRICK_CHIMNEY.get());
+            event.accept(SootyChimneys.Items.IRON_CHIMNEY.get());
+            event.accept(SootyChimneys.Items.DIRTY_IRON_CHIMNEY.get());
+            event.accept(SootyChimneys.Items.COPPER_CHIMNEY.get());
+            event.accept(SootyChimneys.Items.DIRTY_COPPER_CHIMNEY.get());
+            event.accept(SootyChimneys.Items.TERRACOTTA_CHIMNEY.get());
+            event.accept(SootyChimneys.Items.DIRTY_TERRACOTTA_CHIMNEY.get());
+        }
+    }
+
     @SubscribeEvent
     public static void onCommonSetup(FMLCommonSetupEvent event) {
         event.enqueueWork(() -> {
